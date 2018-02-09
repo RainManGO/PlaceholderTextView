55dff7e403f2dc1fc0a21c5165f4ee6ccb3187ca
ZY <776210576@qq.com>
2018-02-09 16:44:02 +0800

    license

--- PlaceholderTextView/ViewController.swift
+++ PlaceholderTextView/ViewController.swift
@@ -12,16 +12,19 @@ import Foundation
 class ViewController: UIViewController {
 
     // 初始化placeholderTextView
-//    lazy var placeholderTextView = PlaceholerTextView(placeholder: "please input", placeholderColor: UIColor.lightGray, frame: CGRect(x: 40, y: 100, width: 320, height: 200))
+    lazy var placeholderTextView = PlaceholerTextView(placeholder: "please input", placeholderColor: UIColor.lightGray, frame: CGRect(x: 40, y: 100, width: 320, height: 200))
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        //设置是否显示计算label
-//        placeholderTextView.isShowCountLabel = true
-//        //限制字数
-//        placeholderTextView.limitWords = 1000
-//        view.addSubview(placeholderTextView)
+        //设置是否显示计算label
+        placeholderTextView.isShowCountLabel = true
+        //限制字数
+        placeholderTextView.limitWords = 1000
+        //是否return关闭键盘
+        placeholderTextView.isReturnHidden = true
+        
+        view.addSubview(placeholderTextView)
         
     }
 
